ab214cc53e5219a0e50c7eedbcc1237a095ee65c
selena <cyke0315@gmail.com>
2024-06-14 22:10:56 +0800

    fix:修复跨域配置

--- src/main/java/xyz/ontip/filter/HeaderFilter.java
+++ src/main/java/xyz/ontip/filter/HeaderFilter.java
@@ -7,8 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class HeaderFilter implements Filter {
+
+    private static final List<String> ALLOWED_ORIGINS = Arrays.asList(
+            "http://localhost:5173",
+            "http://127.0.0.1:5173"
+    );
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -16,15 +25,20 @@ public class HeaderFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest)req;
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-        response.setHeader("Access-Control-Allow-Origin","http://127.0.0.1:5173");
+
+        String origin = request.getHeader("Origin");
+        if (ALLOWED_ORIGINS.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "86400");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("XDomainRequestAllowed", "1");
+
         filterChain.doFilter(request, response);
     }
 
@@ -33,4 +47,5 @@ public class HeaderFilter implements Filter {
 
     }
 }
+
 
